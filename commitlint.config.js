b68ef0cce2d371394f4add3a67486f09189fe8b1
chunghyun <zzgh06@naver.com>
2026-05-14 14:56:23 +0900

    chore: commitlint 본문 최소 2줄 규칙 추가
    - body-min-lines 커스텀 플러그인 규칙 추가
    - 본문 없거나 1줄이면 커밋 차단

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,21 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'body-min-lines': ({ body }) => {
+          const lines = body ? body.split('\n').filter((line) => line.trim()).length : 0;
+          return [lines >= 2, '본문은 최소 2줄 이상 작성해야 합니다'];
+        },
+      },
+    },
+  ],
   rules: {
     'type-enum': [2, 'always', [
       'feat', 'fix', 'docs', 'style', 'refactor',
       'test', 'chore', 'design', 'comment', 'remove', 'rename'
     ]],
     'subject-max-length': [2, 'always', 50],
+    'body-min-lines': [2, 'always'],
   }
 }
